c760603f850a2c5dd67797f5d058b1dd2c703489
David Avila <davidavila6002@gmail.com>
2022-08-20 00:29:46 -0500

    Welcome Screen added

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import WelcomeScreen from "./src/Screens/Welcome/WelcomeScreen";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <WelcomeScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/Screens/Welcome/WelcomeScreen.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import Welcome from "../../Images/Drawings/welcome.svg";
+import { useFonts } from "expo-font";
+
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  StatusBar,
+} from "react-native";
+const WelcomeScreen = () => {
+  //Cargar fuentes
+  const [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("../../../assets/Roboto-Bold.ttf"),
+    "Hind-Regular": require("../../../assets/Hind-Regular.ttf"),
+    "Roboto-Regular": require("../../../assets/Roboto-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={[styles.container, { paddingTop: StatusBar.currentHeight }]}>
+      <Welcome width={333} height={251} style={styles.imagen} />
+      <Text style={styles.titulo}>My Tasks!</Text>
+      <Text style={styles.frase}>
+        ¡Organiza tus tareas y ahorra más tiempo!
+      </Text>
+
+      <TouchableOpacity style={styles.boton}>
+        <Text style={styles.textBoton}>Iniciar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  imagen: {
+    marginTop: 32,
+  },
+  titulo: {
+    fontSize: 36,
+    marginTop: 26,
+    color: "#525252",
+    fontFamily: "Roboto-Bold",
+  },
+  frase: {
+    marginTop: 33,
+    fontFamily: "Hind-Regular",
+    fontSize: 20,
+    width: 262,
+    textAlign: "center",
+  },
+  textBoton: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 24,
+    color: "#fff",
+  },
+
+  boton: {
+    bottom: 0,
+    position: "absolute",
+    marginBottom: 87,
+    width: 176,
+    height: 55,
+    backgroundColor: "#FF5858",
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default WelcomeScreen;
